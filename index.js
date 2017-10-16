2d9a8a757abec1742352be14daf3581bda2e8e38
Matteo Manchi <matteo.manchi@gmail.com>
2017-10-16 23:39:05 +0200

    Move bot logic

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-import {saveParsedEvent, checkUnparsedEvents} from './db';
-import {configToEvents} from './utils/fetch-events';
-import postEvent from './utils/discord-sender';
-import config from './config.json';
+import bot from './bot';
 
-async function removeEventsAlreadyParsed(events) {
-    const checked = await Promise.all(
-        events.map(event => checkUnparsedEvents(event))
-    );
-
-    return checked.filter(c => !!c);
-}
-
-async function runBot() {
-    // const responses = await Promise.all([
-    //     fetchEvents([Types.GRAND_PRIX, Types.PRO_TOUR]),
-    //     fetchEvents(undefined, [Countries.ITALY_AND_MALTA]),
-    // ]);
-
-    const responses = await Promise.all(configToEvents(config.events));
-
-    // flat array
-    let events = responses.reduce((acc, arr) => acc.concat(arr), []);
-
-    // Get only events not already sent
-    events = await removeEventsAlreadyParsed(events);
-
-    // Send items
-    await Promise.all(events.map(postEvent));
-
-    await events.map(saveParsedEvent);
-}
-
-runBot();
+bot();
