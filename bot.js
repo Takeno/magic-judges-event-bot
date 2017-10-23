5326ab95eb76ba3602bc0bb03ecfb52159c65119
Matteo Manchi <matteo.manchi@gmail.com>
2017-10-23 18:15:17 +0200

    Handle errors

--- bot.js
+++ bot.js
@@ -17,20 +17,23 @@ export default (async function runBot() {
     //     fetchEvents([Types.GRAND_PRIX, Types.PRO_TOUR]),
     //     fetchEvents(undefined, [Countries.ITALY_AND_MALTA]),
     // ]);
+    try {
+        const responses = await Promise.all(configToEvents(config.events));
+        // flat array
+        let events = responses.reduce((acc, arr) => acc.concat(arr), []);
 
-    const responses = await Promise.all(configToEvents(config.events));
-    // flat array
-    let events = responses.reduce((acc, arr) => acc.concat(arr), []);
+        logger.info('Parsed %d events from site', events.length);
 
-    logger.info('Parsed %d events from site', events.length);
+        // Get only events not already sent
+        events = await removeEventsAlreadyParsed(events);
 
-    // Get only events not already sent
-    events = await removeEventsAlreadyParsed(events);
+        logger.info('There are %d new events never sent yet', events.length);
 
-    logger.info('There are %d new events never sent yet', events.length);
+        // Send items
+        await Promise.all(events.map(postEvent));
 
-    // Send items
-    await Promise.all(events.map(postEvent));
-
-    await events.map(saveParsedEvent);
+        await Promise.all(events.map(saveParsedEvent));
+    } catch (e) {
+        logger.error(e);
+    }
 });
